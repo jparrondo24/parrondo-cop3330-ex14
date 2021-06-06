152d7e75fa3dbb933491e11621c717961467dd92
Justin Parrondo <justinparrondo@gmail.com>
2021-06-06 01:39:07 -0400

    Initial commit

---
+++ src/main/java/org/cop3330/App.java
@@ -0,0 +1,25 @@
+package org.cop3330;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is the order amount? ");
+        double amount = in.nextDouble();
+        in.nextLine();
+
+        System.out.print("What is the state? ");
+        String state = in.nextLine().toLowerCase();
+
+        if (state.equals("wi")) {
+            double tax = 0.055 * amount;
+            System.out.printf("The subtotal is $%.2f.\n", amount);
+            System.out.printf("The tax is $%.2f.\n", tax);
+            amount += tax;
+        }
+
+        System.out.printf("The total is $%.2f.\n", amount);
+    }
+}
